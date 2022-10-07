122814765dd18b77fad3b0975448a23ca03f7dbc
Matteo Fusillo <58771053+mfusillo98@users.noreply.github.com>
2022-10-07 16:19:54 +0200

    + Fix autoloaders order

--- php/FuxFramework/autoloaders.php
+++ php/FuxFramework/autoloaders.php
@@ -1,6 +1,33 @@
 <?php
 
 
+/* ##########################
+ * Autoloader per i models
+ * ########################## */
+$_MODELS_FILESYSTEM_TREE = null;
+spl_autoload_register(function ($className) {
+    global $_MODELS_FILESYSTEM_TREE;
+    if (strpos($className, "App\Models") !== false) { //Model con namespace PSR
+        $relativeClassPath = str_replace("App/Models", "", str_replace("\\", "/", $className));
+        $filePath = __DIR__ . "/../../models/$relativeClassPath.php";
+        if (file_exists($filePath)) {
+            include_once $filePath;
+        } else {
+            throw new Exception("FuxAutoloaderException: Cannot autoload app class $className. File $filePath doesn't exists.");
+        }
+    } elseif (strpos($className, "Model") !== false) { //Model con scansione della directory "models"
+        $files = $_MODELS_FILESYSTEM_TREE ?? rglob(PROJECT_ROOT_DIR . "/models/*.php");
+        foreach ($files as $filePath) {
+            $fileName = basename($filePath);
+            if ($fileName === "$className.php") {
+                include_once $filePath;
+                break;
+            }
+        }
+        if (!$_MODELS_FILESYSTEM_TREE) $_MODELS_FILESYSTEM_TREE = $files;
+    }
+});
+
 
 /* ##########################
  * Autoloader per i file nella cartella \App
@@ -72,34 +99,6 @@ spl_autoload_register(function ($className) {
         if (!$_CONTROLLERS_FILESYSTEM_TREE) $_CONTROLLERS_FILESYSTEM_TREE = $files;
     }
 });
-
-/* ##########################
- * Autoloader per i models
- * ########################## */
-$_MODELS_FILESYSTEM_TREE = null;
-spl_autoload_register(function ($className) {
-    global $_MODELS_FILESYSTEM_TREE;
-    if (strpos($className, "App\Models") !== false) { //Model con namespace PSR
-        $relativeClassPath = str_replace("App/Models", "", str_replace("\\", "/", $className));
-        $filePath = __DIR__ . "/../../models/$relativeClassPath.php";
-        if (file_exists($filePath)) {
-            include_once $filePath;
-        } else {
-            throw new Exception("FuxAutoloaderException: Cannot autoload app class $className. File $filePath doesn't exists.");
-        }
-    } elseif (strpos($className, "Model") !== false) { //Model con scansione della directory "models"
-        $files = $_MODELS_FILESYSTEM_TREE ?? rglob(PROJECT_ROOT_DIR . "/models/*.php");
-        foreach ($files as $filePath) {
-            $fileName = basename($filePath);
-            if ($fileName === "$className.php") {
-                include_once $filePath;
-                break;
-            }
-        }
-        if (!$_MODELS_FILESYSTEM_TREE) $_MODELS_FILESYSTEM_TREE = $files;
-    }
-});
-
 
 
 /* ##########################
