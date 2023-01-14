3c45a7ccc3f9afa5cfaa8f133b82003f80f2e518
Matteo Fusillo <58771053+mfusillo98@users.noreply.github.com>
2023-01-14 07:38:05 +0100

    + Added package var read support

--- php/FuxFramework/helpers.php
+++ php/FuxFramework/helpers.php
@@ -522,3 +522,11 @@ function print_r_pre($value)
     print_r($value);
     echo "</pre>";
 }
+
+$_PACKAGES_MANIFEST_FILES = [];
+function getPackageVar($var, $package){
+    global $_PACKAGES_MANIFEST_FILES;
+    $manifest = $_PACKAGES_MANIFEST_FILES[$package] ?? include PROJECT_ROOT_DIR."/app/Packages/$package/manifest.php";
+    $_PACKAGES_MANIFEST_FILES[$package] = $manifest;
+    return $manifest[$var] ?? null;
+}
